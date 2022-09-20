fc29c771f794f9aec4f44a81615b1769584e6c19
Quang Hung Pham <hungpham307@gmail.com>
2022-09-20 22:33:05 +0700

    change toast background color based on mode

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="bg-slate-50 dark:bg-darkBlue min-h-screen">
-      <ToastContainer autoClose={2000} theme='light' />
+      <ToastContainer autoClose={1500} theme={`${lightMode ? 'light' : 'dark'}`} />
       <div className={`${lightMode ? "bg-desktop-light" : "bg-desktop-dark"} bg-cover bg-no-repeat h-48 md:h-60`}>
       </div>
       <div className="max-w-2xl mx-auto -mt-48 md:-mt-64 pt-12 md:pt-16 px-6">
